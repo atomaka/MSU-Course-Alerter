80bdf9c094325e84efb5ff20f07a5d0b9ab754e9
Andrew Tomaka <atomaka@gmail.com>
2013-03-29 19:39:35 -0400

    Initial commit of clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MsuCourseAlerter::Application.config.secret_token = '527dd3fbf9a202940eb0d5ca2fa0efc0260855e2d05798eccdc595e2ece555313058b37b6a850c6b5816c6767dc0b8b27ac10a1a99de43073f596d4a67fab55e'

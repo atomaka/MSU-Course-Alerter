96e5e35d7c677afbb59c4b9105ae4c9db777be17
Andrew Tomaka <atomaka@gmail.com>
2013-04-02 03:07:11 -0400

    Validation and simple functions

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,3 +1,36 @@
 class Alert < ActiveRecord::Base
   attr_accessible :alerted, :course, :department, :user_id
+
+  validates :department,  :presence     => true,
+                          :length       => {
+                            :minimum        => 2,
+                            :maximum        => 4
+                          },
+                          :format       => {
+                            :with           => /\A[A-Za-z]+\Z/
+                          }
+  validates :course,      :presence     => true,
+                          :length       => {
+                            :minimum        => 3,
+                            :maximum        => 4
+                          },
+                          :format       => {
+                            :with           => /\A[0-9]+[A-Za-z]?\Z/
+                          }
+  validates :user_id,     :presence     => true,
+                          :numericality => {
+                            only_integer:   true
+                          }
+
+  def alerted?
+    @alerted
+  end
+
+  def class_name
+    @department + ' ' + @course
+  end
+
+  def sections
+
+  end
 end
